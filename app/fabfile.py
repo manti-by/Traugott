2531d3ecf74065afd578d690eb8e3703e961e010
Alexander Chaika <manti.by@gmail.com>
2016-10-28 23:26:00 +0300

    Update deployment

--- app/fabfile.py
+++ app/fabfile.py
@@ -1,7 +1,9 @@
-from fabric.api import task
-from fabric.operations import run, sudo
+from fabric.api import task, prefix
 from fabric.state import env
+from fabric.operations import run, sudo, put
+from fabric.contrib.files import exists
 from fabric.context_managers import cd, settings
+from contextlib import contextmanager
 
 
 def set_defaults():
@@ -11,6 +13,13 @@ def set_defaults():
         env.app_path = '/home/%s/www/%s/src/app' % (env.user, env.app_name)
     if 'root_path' not in env.keys():
         env.root_path = '/home/%s/www/%s/src' % (env.user, env.app_name)
+
+
+@contextmanager
+def virtualenv():
+    with prefix('source %s/../venv/bin/activate' % env.root_path):
+        yield
+
 
 @task
 def m53():
@@ -52,22 +61,33 @@ def create_db():
         sudo('psql -c "GRANT ALL PRIVILEGES ON DATABASE %s TO %s;"' % (env.app_name, env.app_name), user='postgres')
 
 
-@task
-def pip_install():
-    with cd(env.app_path):
-        sudo('pip install -r requirements.txt')
-
-
 @task(default=True)
 def pull(branch='master'):
-    with settings(warn_only=True):
-        run('cp %s/deploy/keys/deploy ~/.ssh/' % env.root_path)
-        run('cp %s/deploy/confs/github.conf ~/.ssh/config' % env.root_path)
+    if not exists('~/.ssh/deploy'):
+        put('../deploy/keys/deploy', '~/.ssh/deploy')
+        put('../deploy/confs/github.conf', '~/.ssh/config')
+        run('chmod 400 ~/.ssh/deploy')
+        run('ssh-keyscan github.com >> ~/.ssh/known_hosts')
 
-    run('chmod 400 ~/.ssh/deploy')
-    run('ssh-keyscan github.com >> ~/.ssh/known_hosts')
+        run('mkdir %s' % env.root_path)
+        run('git clone git@github.com:manti-by/Traugott.git %s' % env.root_path)
+
+        with cd('../' % env.root_path):
+            run('mkdir log')
+            run('chmod 777 log/')
+
     with cd(env.root_path):
+        run('git checkout %s' % branch)
         run('git pull origin %s' % branch)
+
+
+@task
+def pip_install():
+    if not exists('%s/../venv/' % env.root_path):
+        with cd('%s/../' % env.root_path):
+            run('virtualenv --no-site-packages --prompt="%s" venv' % env.app_name)
+    with virtualenv(), cd(env.app_path):
+            run('pip install -r requirements.txt')
 
 
 @task
@@ -85,13 +105,13 @@ def setup():
 
 @task
 def migrate():
-    with cd(env.app_path):
+    with virtualenv(), cd(env.app_path):
         run('python manage.py migrate')
 
 
 @task
 def collect_static():
-    with cd(env.app_path):
+    with virtualenv(), cd(env.app_path):
         run('python manage.py collectstatic --noinput')
 
 
@@ -104,8 +124,8 @@ def reload_app():
 
 @task
 def deploy():
-    pip_install()
     pull()
+    pip_install()
     migrate()
     collect_static()
     reload_app()
@@ -115,8 +135,8 @@ def deploy():
 def deploy_all():
     provision()
     create_db()
-    pip_install()
     pull()
+    pip_install()
     setup()
     migrate()
     collect_static()
