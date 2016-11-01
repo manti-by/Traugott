62e1e2519255707c2b7ec3a7923a20257ba14c3d
Alexander Chaika <manti.by@gmail.com>
2016-11-01 16:58:38 +0300

    Show shots on front page

--- app/shots/models.py
+++ app/shots/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.staticfiles.templatetags.staticfiles import static as static_file
 
 from traugott.mixins import ImageMixin
 
@@ -32,3 +33,11 @@ class Shot(models.Model):
 
     def __str__(self):
         return '%s %s' % (self.created.strftime('%d/%m %H:%M'), self.type.title)
+
+    def as_dict(self):
+        if self.type.image:
+            image_url = self.type.image.url
+        else:
+            image_url = static_file('img/no-image.png')
+        return {'date': self.created.strftime('%d/%m %H:%M'), 'image': image_url,
+                'text': '%s, %sml' % (self.type.title, self.type.volume)}
