97b47f44c7edcc9bbc23cd76fe2d3477cc77722e
Alexander Chaika <manti.by@gmail.com>
2016-11-01 16:21:02 +0300

    Add image mixin to shots model

--- app/shots/models.py
+++ app/shots/models.py
@@ -2,10 +2,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from traugott.mixins import ImageMixin
 
-class ShotType(models.Model):
+
+class ShotType(ImageMixin, models.Model):
     title = models.CharField(max_length=10)
-    image = models.ImageField(upload_to='shot', blank=True, null=True)
     volume = models.IntegerField(default=100)
     degree = models.IntegerField(default=40)
 
