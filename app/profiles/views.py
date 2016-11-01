dfe768e4386664e1ba3ce2c4346bb99e6a4c75c6
Alexander Chaika <manti.by@gmail.com>
2016-11-01 15:37:19 +0300

    Remove ajax and SPA skeleton, use pure django templates

--- app/profiles/views.py
+++ app/profiles/views.py
@@ -1,49 +1,39 @@
 import logging
 
+from django.http import Http404
+from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 
 from profiles.models import Profile
-from traugott.resources import Rest
 
 logger = logging.getLogger('app')
 
 
-class ProfileResource(Rest):
-
-    def get(self, request, profile_id=None):
-        if profile_id:
-            try:
-                profile = Profile.objects.get(pk=profile_id)
-                return self.response({'status': 200,
-                                      'message': 'OK',
-                                      'data': profile.as_dict()})
-            except Profile.DoesNotExist:
-                return self.response({'status': 404,
-                                      'message': 'Profile #%s not found' % profile_id})
-            except Exception as e:
-                return self.response({'status': 500,
-                                      'message': e.message})
-        elif request.user.is_authenticated:
-            return self.response({'status': 200,
-                                  'data': request.user.profile.as_dict()})
-        return self.response({'status': 403,
-                              'message': 'Invalid profile id or not loggined'})
-
-    def post(self, request):
+def login_page(request):
+    try:
         if request.user.is_authenticated:
-            return self.response({'status': 205,
-                                  'message': 'Already loggined'})
-        try:
+            return redirect('index')
+
+        if request.method == 'POST':
+            if not request.POST.get('email') or not request.POST.get('password'):
+                return render(request, 'profiles/login.html',
+                              {'error': 'Please fill email and password fields'})
+
             profile = Profile.get_or_create(request.POST.get('email'),
                                             request.POST.get('password'))
             if not profile:
-                return self.response({'status': 401,
-                                      'message': 'Invalid password'})
-            login(request, profile.user)
-        except Exception as e:
-            return self.response({'status': 500, 'message': e.message})
-        return self.response({'status': 200, 'message': 'OK'})
+                return render(request, 'profiles/login.html',
+                              {'error': 'Invalid password'})
+            else:
+                login(request, profile.user)
+                return redirect('index')
 
-    def delete(self, request):
-        logout(request)
-        return self.response({'status': 200, 'message': 'OK'})
+        return render(request, 'profiles/login.html')
+    except Exception as e:
+        logger.exception(e)
+        raise Http404
+
+
+def logout_page(request):
+    logout(request)
+    return redirect('index')
