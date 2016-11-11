f8878bd0999b34171ff48040d26833e010c20203
Alexander Chaika <manti.by@gmail.com>
2016-11-11 18:28:33 +0300

    Update models and admin page

--- app/shots/admin.py
+++ app/shots/admin.py
@@ -3,53 +3,78 @@ from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.staticfiles.templatetags.staticfiles import static as static_file
 
-from shots.models import ShotType, Shot
+from shots.models import ShotIcon, ShotType, Shot
 from churchill.mixins import IMAGES_HELP_TEXT
+
+
+@admin.register(ShotIcon)
+class ShotIconAdmin(admin.ModelAdmin):
+
+    list_display = ('icon_image', 'title')
+    ordering = ('title',)
+
+    fieldsets = (
+        (_('Info'), {
+            'fields': ('title',)
+        }),
+        (_('Images'), {
+            'fields': ('image', 'thumb', 'preview'),
+            'description': '<div class="help">%s</div>' % IMAGES_HELP_TEXT,
+        }),
+    )
+
+    def icon_image(self, obj):
+        icon = static_file('img/no-image.png')
+        if obj.thumb:
+            icon = obj.thumb.url
+        return mark_safe('<img src="%s" width="50" />' % icon)
+    icon_image.short_description = _('Icon')
 
 
 @admin.register(ShotType)
 class ShotTypeAdmin(admin.ModelAdmin):
 
-    list_display = ('thumb_image', 'title', 'volume', 'degree')
+    list_display = ('icon_image', 'title', 'volume', 'measure', 'degree')
     ordering = ('title',)
 
     fieldsets = (
         (_('Info'), {
-            'fields': ('title', 'volume', 'degree')
+            'fields': ('title', ('volume', 'measure'), 'degree')
         }),
-        (_('Images'), {
-            'classes': ('grp-collapse grp-closed',),
-            'fields': ('image', 'thumb', 'preview'),
-            'description': '<div class="help">%s</div>' % IMAGES_HELP_TEXT,
-        })
+        (_('Externals'), {
+            'fields': ('icon', 'user', 'is_public')
+        }),
     )
 
-    def thumb_image(self, obj):
-        image = static_file('img/no-image.png')
-        if obj.thumb:
-            image = obj.thumb.url
-        return mark_safe('<img src="%s" width="50" />' % image)
-    thumb_image.short_description = _('Image')
+    def icon_image(self, obj):
+        icon = static_file('img/no-image.png')
+        if obj.icon and obj.icon.thumb:
+            icon = obj.icon.thumb.url
+        return mark_safe('<img src="%s" width="50" />' % icon)
+    icon_image.short_description = _('Icon')
 
 
 @admin.register(Shot)
 class ShotAdmin(admin.ModelAdmin):
 
-    list_display = ('thumb_image', 'created', 'user', 'title', 'volume', 'degree')
+    list_display = ('icon_image', 'created', 'user', 'title', 'volume', 'measure', 'degree')
     ordering = ('created',)
 
-    def thumb_image(self, obj):
-        image = static_file('img/no-image.png')
-        if obj.type.thumb:
-            image = obj.type.thumb.url
-        return mark_safe('<img src="%s" width="50" />' % image)
-    thumb_image.short_description = _('Image')
+    def icon_image(self, obj):
+        icon = static_file('img/no-image.png')
+        if obj.type.icon and obj.type.icon.thumb:
+            icon = obj.type.icon.thumb.url
+        return mark_safe('<img src="%s" width="50" />' % icon)
+    icon_image.short_description = _('Icon')
 
     def title(self, obj):
         return obj.type.title
 
     def volume(self, obj):
         return obj.type.volume
+
+    def measure(self, obj):
+        return obj.type.measure
 
     def degree(self, obj):
         return obj.type.degree
