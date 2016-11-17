d903096149285dbd7bc6811e04d78f3292050db7
Alexander Chaika <manti.by@gmail.com>
2016-11-17 14:44:37 +0300

    Fix image mixin, update render behaviour

--- app/churchill/static/js/shots/shot-type.view.js
+++ app/churchill/static/js/shots/shot-type.view.js
@@ -17,16 +17,19 @@
                 add_button = add_shot_type_dialog.find('.add'),
                 close_button = add_shot_type_dialog.find('.close');
 
+            open_button.unbind('click');
             open_button.on('click', function () {
                 add_shot_dialog.addClass('hidden');
                 add_shot_type_dialog.removeClass('hidden');
             });
 
+            add_button.unbind('click');
             add_button.on('click', function () {
                 var result = {
                     title: add_shot_type_dialog.find('#title').val(),
                     volume: parseInt(add_shot_type_dialog.find('#volume').val()),
-                    degree: parseInt(add_shot_type_dialog.find('#degree').val())
+                    degree: parseInt(add_shot_type_dialog.find('#degree').val()),
+                    icon: parseInt(add_shot_type_dialog.find('.icon:checked').val())
                 };
 
                 $.shotTypeModel.create(result, function (data) {
@@ -34,9 +37,11 @@
 
                     add_shot_dialog.removeClass('hidden');
                     add_shot_type_dialog.addClass('hidden');
+                    $.shotView.initAddDialog();
                 });
             });
 
+            close_button.unbind('click');
             close_button.on('click', function () {
                 add_shot_type_dialog.addClass('hidden');
             });
