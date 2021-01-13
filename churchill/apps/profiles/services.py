a958d1289a37b2ab6e2e651f42ba940907e23d83
Alexander Chaika <manti.by@gmail.com>
2021-01-14 01:41:31 +0300

    Remove rest auth, add custom account creation, update api routes

--- churchill/apps/profiles/services.py
+++ churchill/apps/profiles/services.py
@@ -1,30 +1,27 @@
 from decimal import Decimal
 from typing import Optional
 
-from django.contrib.auth.models import User
-
 from churchill.apps.profiles.models import Profile
 
 
 def update_profile(
-    user: User,
+    profile: Profile,
     language: Optional[str] = None,
     currency: Optional[str] = None,
     avg_consumption: Optional[int] = None,
     avg_price: Optional[Decimal] = None,
 ) -> Profile:
     if language is not None:
-        user.profile.language = language
+        profile.language = language
 
     if currency is not None:
-        user.profile.currency = currency
+        profile.currency = currency
 
     if avg_consumption is not None:
-        user.profile.avg_consumption = avg_consumption
+        profile.avg_consumption = avg_consumption
 
     if avg_price is not None:
-        user.profile.avg_price = avg_price
+        profile.avg_price = avg_price
 
-    user.profile.save()
-
-    return user.profile
+    profile.save()
+    return profile
