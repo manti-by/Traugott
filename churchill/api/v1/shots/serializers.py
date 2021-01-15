bd4888e09023c3af7c1e8bd2917da710ee52e4b2
Alexander Chaika <manti.by@gmail.com>
2021-01-15 19:24:33 +0300

    Add shot item api

--- churchill/api/v1/shots/serializers.py
+++ churchill/api/v1/shots/serializers.py
@@ -2,8 +2,15 @@ from rest_framework import serializers
 
 
 class ShotSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(max_length=100)
     volume = serializers.IntegerField()
     degree = serializers.IntegerField(min_value=0, max_value=100)
     cost = serializers.DecimalField(min_value=0, max_digits=5, decimal_places=2)
+    created_at = serializers.DateTimeField(read_only=True)
+
+
+class ShotItemSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    shot = ShotSerializer()
     created_at = serializers.DateTimeField(read_only=True)
--- churchill/apps/shots/admin.py
+++ churchill/apps/shots/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 
-from churchill.apps.shots.models import Shot
+from churchill.apps.shots.models import Shot, ShotItem
 
 
 @admin.register(Shot)
 class ShotAdmin(admin.ModelAdmin):
 
-    list_display = ("title", "volume", "degree", "cost")
+    list_display = ("title", "volume", "degree", "cost", "created_at", "updated_at")
 
     def save_formset(self, request, form, formset, change):
         for f in formset.forms:
@@ -15,3 +15,10 @@ class ShotAdmin(admin.ModelAdmin):
                 obj.created_by = request.user
             obj.save()
         formset.save()
+
+
+@admin.register(ShotItem)
+class ShotItemAdmin(admin.ModelAdmin):
+
+    list_display = ("user", "shot", "created_at", "updated_at")
+    raw_id_fields = ("user", "shot")
