06096f8395c4df1512b8dbf38dad130d079fe4a7
Alexander Chaika <manti.by@gmail.com>
2021-03-09 23:01:41 +0300

    Add balance and shots calculation, update calendars

--- churchill/apps/profiles/services.py
+++ churchill/apps/profiles/services.py
@@ -2,11 +2,12 @@ from datetime import datetime, timedelta
 from decimal import Decimal
 from typing import Optional, Tuple
 
+from django.db.models import Count
 from django.utils import timezone
 
 from churchill.apps.profiles.exception import InvalidStatsCalculationStrategyException
 from churchill.apps.profiles.models import Profile
-from churchill.apps.shots.models import ShotItem
+from churchill.apps.shots.models import ShotItem, Shot
 
 
 def update_profile(
@@ -32,16 +33,31 @@ def update_profile(
     return profile
 
 
+def get_days_to_balance_count(profile: Profile, volume: int) -> int:
+    return int(volume / (profile.avg_consumption / 365) * -1) if volume < 0 else 0
+
+
+def get_popular_drink_string(profile: Profile, money: Decimal) -> str:
+    if money >= 0:
+        return ""
+    shot = Shot.objects.for_user(profile.user).annotate(items_count=Count("items")).order_by("-items_count").first()
+    return f"{round(-1 * money / shot.cost, 1)} shots of {shot.title}"
+
+
 def get_profile_stats(profile: Profile) -> Optional[dict]:
     last_shot = ShotItem.objects.filter(user=profile.user).order_by("created_at").last()
     if last_shot:
         volume, money = calculate_consuming_stats(profile, last_shot)
         timedelta_last_shot = timezone.now() - last_shot.created_at
+        days_to_balance = get_days_to_balance_count(profile, volume)
+        popular_drink_string = get_popular_drink_string(profile, money)
         return {
             "last_shot_at": last_shot.created_at,
             "timedelta_last_shot": timedelta_last_shot,
             "timedelta_last_shot_repr": str(timedelta_last_shot).split(".")[0],
             "skipped_volume_last_shot": volume,
+            "days_to_balance": days_to_balance,
+            "popular_drink": popular_drink_string,
             "money_saved_last_shot": money,
         }
 
